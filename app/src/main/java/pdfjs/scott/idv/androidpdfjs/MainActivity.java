601c4280023c25e3a2efbf0c020fea5b568dcca1
楊慶堂 <scott.yang@ylhealth.org>
2018-03-28 16:47:24 +0800

    add android code

--- app/src/main/java/pdfjs/scott/idv/androidpdfjs/MainActivity.java
+++ app/src/main/java/pdfjs/scott/idv/androidpdfjs/MainActivity.java
@@ -1,13 +1,37 @@
 package pdfjs.scott.idv.androidpdfjs;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WebView mWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+
+        String pdfUrl = "file:///android_asset/pdf-js-demo.pdf";
+        mWebView.loadUrl("file:///android_asset/index.html?" + pdfUrl);
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private void initView() {
+        mWebView = findViewById(R.id.webView);
+
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setAllowFileAccess(true);
+        webSettings.setAllowFileAccessFromFileURLs(true);
+        webSettings.setAllowUniversalAccessFromFileURLs(true);
+        webSettings.setSupportZoom(true);
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setUseWideViewPort(true);
+        webSettings.setBuiltInZoomControls(true);
     }
 }
