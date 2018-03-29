dd1aa1610db2b59ace6ef1f5254b71232440cc8e
楊慶堂 <scott.yang@ylhealth.org>
2018-03-29 09:12:35 +0800

    增加可以 alert

---
+++ README.md
@@ -0,0 +1,4 @@
+參考資料：
+# https://stackoverflow.com/questions/8648616/webview-javascript-cross-domain-from-a-local-html-file
+# 為了讓 alert 可以生效 https://issuetracker.google.com/issues/36905249
+
--- app/src/main/java/pdfjs/scott/idv/androidpdfjs/MainActivity.java
+++ app/src/main/java/pdfjs/scott/idv/androidpdfjs/MainActivity.java
@@ -3,6 +3,7 @@ package pdfjs.scott.idv.androidpdfjs;
 import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 
@@ -17,12 +18,17 @@ public class MainActivity extends AppCompatActivity {
         initView();
 
         String pdfUrl = "file:///android_asset/pdf-js-demo.pdf";
+        // pdfUrl = "http://10.57.104.139/IBM-0308.pdf";
+
         mWebView.loadUrl("file:///android_asset/index.html?" + pdfUrl);
     }
 
     @SuppressLint("SetJavaScriptEnabled")
     private void initView() {
         mWebView = findViewById(R.id.webView);
+        // 為了讓 alert 可以生效
+        // https://issuetracker.google.com/issues/36905249
+        mWebView.setWebChromeClient(new WebChromeClient());
 
         WebSettings webSettings = mWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
